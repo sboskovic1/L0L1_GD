8b0d317c767c07d81e21034cc027d877384b2d27
Cyrus Najmi-Vannini <csn2@rice.edu>
2025-05-03 14:06:14 -0500

    smaller lambda in NGD.

--- normalized_gd.py
+++ normalized_gd.py
@@ -5,19 +5,25 @@ def normalized_gd(f, g, x0, xstar, l0, l1, epsilon, iters):
     # print("running normalized gradient descent")
     error = []
     x = x0
-    error.append((0, np.linalg.norm(x - xstar)))
+    error.append((0, np.linalg.norm(f(x) - f(xstar)))) # function value diff.
+    lambda_param = 0.1
+    beta = 1e-6
 
     # Let's hope I nailed the formula
-    eta = 1 / (2 * (l0 + l1))  # Constant step size. This is correct, right?
+    eta_n = lambda_param / (2 * (l0 + l1 * lambda_param))
     i = 1
     while i < iters and error[-1][1] > epsilon:
         grad = g(x)
-        # Do not divide by zero!
-        if np.linalg.norm(grad) < epsilon:
-            break
-        # Normalize the gradient: grad / ||grad||
-        normalized_grad = grad / np.linalg.norm(grad)
-        x = x - eta * normalized_grad
+        grad_norm = np.linalg.norm(grad)
+        
+        # Check if gradient is really small.
+        if grad_norm < epsilon:
+            break            
+        
+        # Normalized gradient update this had better be right.
+        h_n = eta_n / (grad_norm + beta)
+        x = x - h_n * grad        
+        
         error.append((i, np.linalg.norm(f(x) - f(xstar))))
         i += 1
     return error
